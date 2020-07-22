1a1fe5269cdd6dc65773278198cbfed6d8689098
brad taniguchi <bradtaniguchi@gmail.com>
2020-07-22 00:36:29 -0700

    Add local-forage service and deps
    - will be used to back-up the application data

---
+++ projects/client/src/app/services/local-forage.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import localForage from 'localforage';
+import { from, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalForageService {
+  /**
+   * The underlying instance of localForage.
+   */
+  public instance = localForage;
+  public ready$ = from(localForage.ready);
+  constructor() {
+    localForage.config({
+      driver: localForage.WEBSQL, // Force WebSQL; same as using setDriver()
+      name: 'myApp',
+      version: 1.0,
+      size: 4980736, // Size of database, in bytes. WebSQL-only for now.
+      storeName: 'fcc-markdown-previewer', // Should be alphanumeric, with underscores.
+      description: 'client-side data'
+    });
+  }
+
+  public get<T = any>(key: string): Observable<T> {
+    return from(localForage.getItem(key)) as any;
+  }
+
+  public setItem<T = any>(key: string, value: T): Observable<T> {
+    return from(localForage.setItem(key, value));
+  }
+
+  public removeItem(key: string) {
+    return from(localForage.removeItem(key));
+  }
+
+  public clear() {
+    return from(localForage.clear());
+  }
+}
