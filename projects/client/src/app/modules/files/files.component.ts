e97ce738747d7d262ded2c6c2e94036641edd1a6
brad taniguchi <bradtaniguchi@gmail.com>
2020-07-26 12:46:23 -0700

    Start adding logic for files page
    - will show the user files, favorites, and other logic to manage files.

--- projects/client/src/app/modules/files/files.component.ts
+++ projects/client/src/app/modules/files/files.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { File } from '../../models/file';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-files',
   template: `
-    <p>
-      files works!
-    </p>
+    <mat-list>
+      <mat-list-item *ngFor="let file of files">
+        <div matLine>
+          <span>
+            <!-- TODO: add checkbox -->
+          </span>
+          <span>
+            {{ file.name }}
+          </span>
+          <span>
+            <!-- TODO: add menu button -->
+          </span>
+        </div>
+      </mat-list-item>
+    </mat-list>
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FilesComponent implements OnInit {
+  public files$!: Observable<File[]>;
   constructor() {}
 
   ngOnInit(): void {}
